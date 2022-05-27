819205c19afc8adafa141433934b62e88fe2d67f
kelly kiiru <kelly.kiiru@student.moringaschool.com>
2022-05-27 18:19:14 +0300

    add location model; register models in admin;

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
-
+from .models import category,Location,Pin
 # Register your models here.
+
+class PinAdmin(admin.ModelAdmin):
+    filter_horizontal=('category',)
+
+admin.site.register(Pin,PinAdmin)
+admin.site.register(Location)
+admin.site.register(category)
--- main/models.py
+++ main/models.py
@@ -1,8 +1,37 @@
+from enum import unique
 from django.db import models
 
+
 # Create your models here.
+class Location(models.Model):
+    location = models.CharField(max_length=40)
+    location_list=[]
+    
+    def __str__(self):
+        return self.location
+    
+    def save_location(self):
+        self.save()
+    
+
+class category(models.Model):
+    category_name=models.CharField(max_length=40)
+    category_list=[]
+    
+    def __str__(self):
+        return self.category_name
+    
+    def save_category(self):
+        self.save()
 
 class Pin(models.Model):
     pin_name=models.CharField(max_length=30)
     pin_description=models.CharField(max_length=100)
-    pin_image=models.ImageField(upload_to='pins/')
+    category=models.ManyToManyField(category)
+    pin_image=models.ImageField(upload_to='pins/', unique=True)
+    
+    def save_pin(self):
+        self.save()
+        
+    class Meta:
+        ordering = ['pin_name']
