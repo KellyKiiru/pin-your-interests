45ef844a3b7eb7d76282ab4b6079690b0524fe95
kelly kiiru <kelly.kiiru@student.moringaschool.com>
2022-05-28 19:03:58 +0300

    add settings:DEBUG in main/urls;add bootstrap cdn;display images in index page; delete navbar and navbar styling

--- main/urls.py
+++ main/urls.py
@@ -1,8 +1,13 @@
 from . import views
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns=[
     path('',views.homepage,name='homepage'),
     path('location/<pin_id>/',views.search_pin_by_location, name='search_pin_by_location'),
     path('category/<pin_id>',views.search_pin_by_category,name='search_pin_by_location'),    
 ]
+
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
