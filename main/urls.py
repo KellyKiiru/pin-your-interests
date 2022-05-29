94f97457c77f232ead053dbb778842d460b71e29
kelly kiiru <kelly.kiiru@student.moringaschool.com>
2022-05-29 14:07:23 +0300

    fix path for search to display pins by category

--- main/urls.py
+++ main/urls.py
@@ -6,8 +6,7 @@ from django.conf.urls.static import static
 urlpatterns=[
     path('',views.homepage,name='homepage'),
     path('pin/<int:pin_id>',views.show_single_pin, name='show_single_pin'),
-    path('category/<pin_id>',views.search_pin_by_category,name='search_pin_by_location'),
-    path('search/', views.location,name='search_results'),
+    path('search/', views.search_pin_by_category,name='search_results'),
     path('location/<location>', views.location,name='location'),
     
 ]
